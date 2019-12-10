8d762805c3a47174e6a0eaae1c1a537e499ba509
Kenneth Lee <KennethVLee@gmail.com>
2019-12-10 08:32:01 -0500

    Initial Commit

---
+++ FantasyFootballIOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FantasyFootballIOS
+//
+//  Created by Lee, Kenneth Van on 12/10/19.
+//  Copyright © 2019 Lee, Kenneth Van. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
